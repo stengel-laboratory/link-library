bdcfd826374e16073928d955101dd21d608647cb
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2019-01-24 17:07:01 +0100

    Can now plot log2ratios, dilution series and link overviews

--- link_library/bag_container_library/plot_bag.py
+++ link_library/bag_container_library/plot_bag.py
@@ -2,8 +2,11 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import link_library.plot_library as plib
+import link_library as ll
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 from scipy.stats import norm
+from functools import reduce
 
 class PlotMaster(object):
 
@@ -13,13 +16,13 @@ class PlotMaster(object):
 
     def plot_clustermap(self):
         # how far up we want to sum up values; done first (understand it as an exclusion list)
-        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep] # self.bag_cont.col_charge self.bag_cont.col_weight_type
+        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep] # self.bag_cont.col_tech_rep, self.bag_cont.col_charge self.bag_cont.col_weight_type
         # how far do we take the mean of values; done second
         mean_list = sum_list
         # the following two lines will take the mean of the tech replicates; should always be used if don't want separate tech reps
         # mean_list = list(sum_list)
         # mean_list.remove(self.bag_cont.col_tech_rep)
-        df = self.bag_cont.get_pivot(sum_list, mean_list, [self.bag_cont.col_level])
+        df = self.bag_cont.get_pivot(sum_list, mean_list, pivot_on=self.bag_cont.col_area_sum_total)
         df_links = pd.DataFrame(self.bag_cont.df_orig)
         df_links = df_links.groupby([self.bag_cont.col_level, self.bag_cont.col_link_type])[self.bag_cont.col_area_sum_total].sum().reset_index()
         df_links = df_links.set_index(self.bag_cont.col_level)
@@ -27,7 +30,7 @@ class PlotMaster(object):
         lut = dict(zip(df_links.unique(), "cb"))
         row_colors = df_links.map(lut)
         # impute na values with a downshifted normal distribution
-        df, dist_orig, dist_imputed = self.bag_cont.log2_and_fillna_with_normal_dist(df)
+        df, dist_orig, dist_imputed = self.bag_cont.fillna_with_normal_dist(df)
         sns.distplot(dist_orig, kde=True, fit=norm)
         self.plot_fig(name='cluster', extra="orig_dist")
         plt.clf()
@@ -38,17 +41,17 @@ class PlotMaster(object):
         ax = cg.ax_heatmap
         # hiding the y labels as they are really not informative
         ax.tick_params(axis='y', which='both', right=False, labelright=False)
-        self.plot_fig(name="cluster")
+        self.plot_fig(name="cluster", g=cg)
         cg = sns.clustermap(data=df,cmap="mako_r", z_score=0, metric='canberra', row_colors=row_colors)
         ax = cg.ax_heatmap
         # hiding the y labels as they are really not informative
         ax.tick_params(axis='y', which='both', right=False, labelright=False)
-        self.plot_fig(name='cluster', extra="z_row")
+        self.plot_fig(name='cluster', extra="z_row", g=cg)
         cg = sns.clustermap(data=df,cmap="mako_r", z_score=1, metric='canberra', row_colors=row_colors)
         ax = cg.ax_heatmap
         # hiding the y labels as they are really not informative
         ax.tick_params(axis='y', which='both', right=False, labelright=False)
-        self.plot_fig(name='cluster', extra="z_col")
+        self.plot_fig(name='cluster', extra="z_col", g=cg)
 
     def plot_bar(self):
         df = pd.DataFrame(self.bag_cont.df_new)
@@ -62,29 +65,84 @@ class PlotMaster(object):
         df = df.reset_index(drop=True)
         # following line can be used to only plot partial data
         # df = df.loc[df[index_string] < len(df.index)]
-        ax = sns.barplot(x=self.bag_cont.col_level, y=self.bag_cont.col_area_sum_total, hue=self.bag_cont.col_exp, data=df)
+        ax = sns.barplot(x=self.bag_cont.col_level, y=self.bag_cont.col_area_sum_total, hue=self.bag_cont.col_exp, data=df, ci='sd')
         ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
         ax.set(title="{0} col_level".format(self.bag_cont.col_level), yscale='log')
         self.plot_fig(name="bar")
 
     def plot_ms1_area_std(self):
-        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep,self.bag_cont.col_link_type, self.bag_cont.col_tech_rep]
-        mean_list = [self.bag_cont.col_exp,  self.bag_cont.col_link_type]
-        pval_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
-        df_pval = self.bag_cont.get_two_sided_ttest(pval_list, [self.bag_cont.col_exp, self.bag_cont.col_bio_rep])
+        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
+        mean_list = [self.bag_cont.col_exp]
         df = self.bag_cont.get_stats(sum_list, mean_list)
-        df = pd.merge(df, df_pval, on=self.bag_cont.col_level)
+        df = df.sort_values([self.bag_cont.col_exp])
         print(df)
-        fg = sns.relplot(data=df, x='mean', y='std', col=self.bag_cont.col_exp, hue=self.bag_cont.col_link_type)
-        self.plot_fig(name="std")
-        # df = df[(df[self.bag_cont.col_log2ratio] < 1)&(df[self.bag_cont.col_log2ratio] > -1)]
-        print(df)
-        fg = sns.relplot(data=df, y='qval', x='log2ratio', col=self.bag_cont.col_exp, hue='std', palette="brg")
+        fg = sns.relplot(data=df, x='mean', y='std', col=self.bag_cont.col_exp)
         for row in fg.axes:
             for ax in row:
-                ax.hlines(y=0.05, xmin=df[self.bag_cont.col_log2ratio].min(), xmax=df[self.bag_cont.col_log2ratio].max(), label="qval=0.05", colors=['purple'])
-        self.plot_fig(name="ttest")
+                ax.set_ylim(ymin=0)
+                ax.hlines(y=0.5, xmin=df['mean'].min(),
+                          xmax=df['mean'].max(), label="std=0.5", colors=['grey'])
+        self.plot_fig(name="std", g=fg)
+        # df = df[(df[self.bag_cont.col_log2ratio] < 1)&(df[self.bag_cont.col_log2ratio] > -1)]
 
+    def plot_log2ratio(self):
+        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
+        mean_list = [self.bag_cont.col_exp]
+        pval_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
+        exp_ref = ll.input_log2_ref(self.bag_cont.exp_list)
+        df_pval = self.bag_cont.get_two_sided_ttest(pval_list, [self.bag_cont.col_exp, self.bag_cont.col_bio_rep], ref=exp_ref)
+        df_stats = self.bag_cont.get_stats(sum_list, mean_list)
+        df_log2 = self.bag_cont.getlog2ratio(sum_list, mean_list, ref=exp_ref)
+        # n-way merge
+        df = reduce(lambda left, right: pd.merge(left, right, on=[self.bag_cont.col_level, self.bag_cont.col_exp]), [df_stats, df_log2, df_pval])
+        df = df.sort_values([self.bag_cont.col_exp, self.bag_cont.col_level])
+        print(df.groupby(self.bag_cont.col_exp).agg([pd.Series.mean, pd.Series.median]))
+        fg = sns.relplot(data=df, y='qval', x=self.bag_cont.col_log2ratio, col=self.bag_cont.col_exp, palette="brg", hue='std')
+        for row in fg.axes:
+            for ax in row:
+                ax.set_ylim(ymin=0)
+                ax.hlines(y=0.05, xmin=df[self.bag_cont.col_log2ratio].min(),
+                          xmax=df[self.bag_cont.col_log2ratio].max(), label="qval=0.05", colors=['purple'])
+        self.plot_fig(name="log2ratio", g=fg)
+
+    def plot_link_overview(self):
+        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
+        mean_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep]
+        df = self.bag_cont.get_group(sum_list, mean_list)
+        df[self.bag_cont.col_area_sum_total] = np.log2(df[self.bag_cont.col_area_sum_total])
+        # df = self.bag_cont.get_prot_name_and_link_pos(df)
+        # df[self.bag_cont.col_exp+'_sorted'] = pd.Categorical(
+        #     df[self.bag_cont.col_exp],
+        #     categories=['T5', 'T3', 'T2', 'T1', 'T6'],
+        #     ordered=True
+        # )
+        if self.bag_cont.col_domain:
+            df = df.sort_values([self.bag_cont.col_domain, self.bag_cont.col_exp, self.bag_cont.col_level])
+        else:
+            df = df.sort_values([self.bag_cont.col_exp, self.bag_cont.col_level])
+        fg = sns.catplot(data=df, x=self.bag_cont.col_exp, y=self.bag_cont.col_area_sum_total, col=self.bag_cont.col_level, kind='point', col_wrap=5, ci='sd', sharey=False, hue=self.bag_cont.col_domain)
+        self.plot_fig(name="link_overview",g=fg)
+
+    def plot_dilution_series(self):
+        sns.set(font_scale=1.75)
+        sns.set_style("whitegrid")
+        loc = plticker.MultipleLocator(base=1.0)  # this locator puts ticks at regular intervals
+        sum_list = [self.bag_cont.col_exp, self.bag_cont.col_bio_rep, self.bag_cont.col_tech_rep]
+        mean_list = [self.bag_cont.col_exp]
+        df = self.bag_cont.get_group(sum_list, mean_list)
+        df = df.sort_values([self.bag_cont.col_exp, self.bag_cont.col_level])
+        exp_ref = ll.input_log2_ref(self.bag_cont.exp_list)
+        df_log2ratio = self.bag_cont.getlog2ratio(sum_list, mean_list, exp_ref)
+        df[self.bag_cont.col_area_sum_total] = np.log2(df[self.bag_cont.col_area_sum_total])
+        ax = sns.boxplot(data=df, x=self.bag_cont.col_exp, y=self.bag_cont.col_area_sum_total)
+        ax.yaxis.set_major_locator(loc)
+        # ax.set(yscale='log')
+        self.plot_fig(name="dilution_series")
+        ax = sns.boxplot(data=df_log2ratio, x=self.bag_cont.col_exp, y=self.bag_cont.col_log2ratio)
+        ax.yaxis.set_major_locator(loc)
+        print(df.groupby(self.bag_cont.col_exp).agg([pd.Series.mean, pd.Series.median]))
+        print(df_log2ratio.groupby(self.bag_cont.col_exp).agg([np.mean, np.median]))
+        self.plot_fig(name="dilution_series_log2ratio")
 
     def plot_scatter(self):
         df = pd.DataFrame(self.bag_cont.df_new)
@@ -172,6 +230,7 @@ class PlotMaster(object):
                             ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
                     self.plot_fig(name='rep', extra="{0}_vs_{1}".format(bio_rep_dict[bio_rep_outer], bio_rep_dict[bio_rep_inner]))
 
+
     def plot_bio_rep_bar(self):
         df = pd.DataFrame(self.bag_cont.df_orig)
         # filter zero ms1 intensities
@@ -193,10 +252,15 @@ class PlotMaster(object):
             self.plot_fig(name='rep_bar',extra="{0}".format(row))
 
 
-    def plot_fig(self, name, extra=''):
+    def plot_fig(self, name, extra='', g = None):
         filter = ""
         if self.bag_cont.filter:
             filter = '_' + self.bag_cont.filter
         if extra:
             extra = '_' + extra
-        plib.save_fig("bag_{0}_{1}{2}{3}".format(name, self.bag_cont.col_level, filter, extra), self.out_folder)
+        save_string = "bag_{0}_{1}{2}{3}".format(name, self.bag_cont.col_level, filter, extra)
+        if g is None:
+            plib.save_fig(save_string, self.out_folder)
+        else:
+            plib.save_g(g, save_string, self.out_folder)
+        plt.clf()
