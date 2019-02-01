800181d352e4e0adf6762da48485330784461477
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2019-02-01 14:17:54 +0100

    Label points on axes or facetgrids

--- link_library/plot_library.py
+++ link_library/plot_library.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import os
 
 
@@ -23,9 +24,9 @@ def save_fig(out_name, out_dir='plots'):
     plt.savefig("{0}/plot_{1}.png".format(out_dir, out_name))
 
 
-def save_g(fg, out_name, out_dir='plots'):
+def save_g(fg, out_name, out_dir='plots', **kwargs):
     out_dir = create_plots_dir(out_dir)
-    fg.savefig("{0}/plot_{1}.png".format(out_dir, out_name))
+    fg.savefig("{0}/plot_{1}.png".format(out_dir, out_name), **kwargs)
 
 
 def save_n_show_fig(out_name, out_dir = 'plots'):
@@ -43,3 +44,24 @@ def facet_grid_vertical_label(fg, size=None):
     for row in fg.axes:
         for ax in row:
             ax.set_xticklabels(ax.get_xticklabels(), rotation=90, size=size)
+
+def label_point(x, y, val, ax):
+    a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+        ax.text(point['x'], point['y']+0.15, str(point['val']), fontsize=14, fontweight=600)
+
+def map_point(x,y, annot, **kwargs):
+    # x = x.unique()
+    # print(x)
+    # print(y)
+    y_anno_dict = {y.values[n]:annot.values[n] for n in range(len((y)))}
+    print(y_anno_dict)
+    for i, y_val in enumerate(y_anno_dict.keys()):
+        # print(str(annot.values[i]), x.values[i], y.values[i])
+        plt.annotate(str(y_anno_dict[y_val]), xy=(i, y_val), fontsize=8,
+                    color=kwargs.get("color", "k"),
+                    bbox=dict(pad=.9, alpha=1, fc='w', color='none'),
+                    va='center', ha='center', weight='bold')
+
+def horizontal_mean_line(y, **kwargs):
+    plt.axhline(y.mean(), **kwargs)
