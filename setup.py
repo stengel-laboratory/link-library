eeebdd09d5d1be12dd1ed0108df50e20e1e93962
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2019-03-19 14:19:57 +0100

    Added requirements to the package

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 setup(name='link_library',
       description='Management of crosslink databases',
       author='Kai Kammer',
@@ -6,5 +7,9 @@ setup(name='link_library',
       url='https://git.uni-konstanz.de/kai-michael-kammer/link_library',
       version='1.0',
       packages=['link_library'],
+      install_requires=[
+          'matplotlib',
+          'pandas'
+      ],
       license='MIT',
       )
